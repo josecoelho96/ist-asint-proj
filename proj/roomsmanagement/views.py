1ff940404c8d8825915b82f6f73276d0bede0066
xavierreis <xavier.reis1994@gmail.com>
2017-12-18 00:49:31 +0000

    Checkout and checkin method. Get details from room. Room view

--- proj/roomsmanagement/views.py
+++ proj/roomsmanagement/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Room, User
+from .models import Room, User, Entry
 import requests
 from django.http import HttpResponse, JsonResponse
 from .forms import SearchRoomForm
 import time
+from .helper import checkin, get_room_details, checkout
 
 # Create your views here.
 
@@ -77,8 +78,11 @@ def list_rooms(request):
 
 def room_details(request, room_id):
 
-    room = get_object_or_404(Room, pk=room_id)
-    return render(request, 'roomsmanagement/roomdetails.html', {'room': room})
+    room = get_room_details(room_id)
+    checkout('2222', 'ist181017')
+    #return render(request, 'roomsmanagement/roomdetails.html', {'room': room})
+    return render(request, 'roomsmanagement/room.html', {'room': room})
+
 
 
 def login(request):
